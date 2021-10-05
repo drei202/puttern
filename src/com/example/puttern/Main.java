c473184e3cf91a200b2784e178f527dcf6a1f4a5
stnwtr <simon.steinkellner.ss@gmail.com>
2021-10-05 11:02:43 +0200

    Add simple UI/UX

--- src/com/example/puttern/Main.java
+++ src/com/example/puttern/Main.java
@@ -1,9 +1,20 @@
 package com.example.puttern;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Pattern pattern = symbol -> "lol";
+        System.out.println("This program prints a lot of beautiful patterns");
+        System.out.println("Lets get started.");
 
-        pattern.draw('*');
+        var patterns = new ArrayList<Pattern>();
+
+        patterns.add(symbol -> "lol");
+
+        for (int i = 0; i < patterns.size(); i++) {
+            System.out.println(i+1+". Pattern:");
+            patterns.get(i).draw('*');
+            System.out.println();
+        }
     }
 }
