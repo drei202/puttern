d4bccb390fa37304a86c91fff1480e00a6eb0a6b
drei20 <72300647+drei20@users.noreply.github.com>
2021-10-05 11:13:51 +0200

    Create SquarePattern.java

---
+++ src/com/example/puttern/SquarePattern.java
@@ -0,0 +1,8 @@
+public class SquarePattern implements Pattern {
+    @Override
+    public String build(char symbol) {
+        return "*".repeat(9) + '\n' +
+                "*       *\n".repeat(7) +
+                "*".repeat(9) + '\n';
+    }
+}
