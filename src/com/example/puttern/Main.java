9df4191e5e1dacaabc841049ebaf9d1be2f736c6
stnwtr <simon.steinkellner.ss@gmail.com>
2021-10-05 10:53:24 +0200

    Core pattern class

--- src/com/example/puttern/Main.java
+++ src/com/example/puttern/Main.java
@@ -2,6 +2,8 @@ package com.example.puttern;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Pattern pattern = symbol -> "lol";
+
+        pattern.draw('*');
     }
 }
